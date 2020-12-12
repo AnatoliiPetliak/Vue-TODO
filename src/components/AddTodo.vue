5ed4c7ae668a6e92faab04ffa6a62b0901738210
Anatolii Petliak <petliakav@gmail.com>
2020-12-12 23:48:16 +0200

    dodo list completed

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="title" />
+    <button type="submit" class="add">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.title.trim()) {
+        const newTodo = {
+          id: Date.now(),
+          title: this.title,
+          completed: false
+        };
+
+        this.$emit("add-todo", newTodo);
+        this.title = "";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input {
+  width: 400px;
+}
+
+.add {
+  background-color: rgb(57, 204, 44);
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border-color: rgb(24, 26, 26);
+  border-width: 2px;
+}
+</style>
---
+++ src/print.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card flat height="100%">
+    <v-toolbar dense>
+      <div class="body-2">Print Stuff</div>
+      <v-spacer></v-spacer>
+      <v-btn small color="primary" @click="printSection">
+        <v-icon small class="mr-1">print</v-icon>Print
+      </v-btn>
+    </v-toolbar>
+    <v-flex xs12>
+      <div id="printSection">
+        <span v-html="printStuff"></span>
+      </div>
+    </v-flex>
+  </v-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    ...mapActions("myModule", ["fetchPrintStuff"]),
+
+    printSection() {
+      this.$htmlToPaper("printSection");
+    },
+  },
+
+  mounted() {
+    this.fetchPrintStuff();
+
+    // a Vuex action to get stuff to print from server
+  },
+};
+</script>
